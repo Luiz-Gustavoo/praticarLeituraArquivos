e96c4d3605901b4c7e7992bfc4495ba938e91399
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-04 16:59:13 -0300

    Alterar o texto para escrita no arquivo, adicionar a chamada do método leituraScanner

--- src/br/com/praticaleituraarquivos/principal/Principal.java
+++ src/br/com/praticaleituraarquivos/principal/Principal.java
@@ -5,10 +5,11 @@ import java.io.IOException;
 public class Principal {
     public static void main(String[] args) throws IOException {
 
-        ManipularArquivo escreverArquivo = new ManipularArquivo("Teste", "Arquivo.txt");
+        ManipularArquivo escreverArquivo = new ManipularArquivo("O rato roeu a roupa do rei de roma", "Arquivo.txt");
         escreverArquivo.escrita();
 
         escreverArquivo.leituraFileReader();
+        escreverArquivo.leituraScanner();
 
     }
 }
