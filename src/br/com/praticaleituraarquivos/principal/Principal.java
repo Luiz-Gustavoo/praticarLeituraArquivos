faaec59595b33196df139952a3f0b99fa9a351c9
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-04 16:50:38 -0300

    Alterar classe do objeto escrever arquivo, adicionar chamada do método leituraFileReader

--- src/br/com/praticaleituraarquivos/principal/Principal.java
+++ src/br/com/praticaleituraarquivos/principal/Principal.java
@@ -1,12 +1,14 @@
 package br.com.praticaleituraarquivos.principal;
-import br.com.praticaleituraarquivos.modelos.EscreverArquivo;
+import br.com.praticaleituraarquivos.modelos.ManipularArquivo;
 import java.io.IOException;
 
 public class Principal {
     public static void main(String[] args) throws IOException {
 
-        EscreverArquivo escreverArquivo = new EscreverArquivo("Teste", "Arquivo.txt");
+        ManipularArquivo escreverArquivo = new ManipularArquivo("Teste", "Arquivo.txt");
         escreverArquivo.escrita();
+
+        escreverArquivo.leituraFileReader();
 
     }
 }
